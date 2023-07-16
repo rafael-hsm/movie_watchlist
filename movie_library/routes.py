78bf376bbf5f7f74200f899166ad7437fc107140
Rafael Meireles <rafameireles2011@gmail.com>
2023-07-15 22:22:37 -0300

    update class 173

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, request
 
 
 pages = Blueprint(
@@ -12,3 +12,15 @@ def index():
         "index.html",
         title="Movies Watchlist",
     )
+
+
+@pages.get('/toogle-theme')
+def toggle_theme():
+    current_theme = session.get('theme')
+    if current_theme == 'dark':
+        session['theme'] = 'light'
+    else:
+        session['theme'] = 'dark'
+    
+    return redirect(request.args.get('current_page'))
+        
