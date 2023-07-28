c7a9ea717497f44978b4568f1f37158fa8fcaf4b
Rafael Meireles <rafael.meireles.prestserv@petrobras.com.br>
2023-07-27 21:59:45 -0300

    fix start.sh 3

--- gunicorn.py
+++ gunicorn.py
@@ -1,2 +1,2 @@
-bind = "0.0.0.0:8000"  # Endereço e porta para o Gunicorn ouvir as conexões
+# gunicorn.py
 workers = 4  # Número de workers para processamento paralelo (ajuste conforme necessário)
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 python -m pip install --upgrade pip
-
-gunicorn movie_library:create_app --bind 0.0.0.0:8000
+gunicorn "movie_library:create_app()" --bind 0.0.0.0:8000
